5e8841199669de33ab9baf575405bd3b70c467ef
Alex <flores15castilla@gmail.com>
2025-12-01 13:04:56 -0500

    a

--- ejemplo.js
+++ ejemplo.js
@@ -13,7 +13,7 @@ function cambiarColor(){
 
 }
 function cambiarLogo(){
-    document.getElementById('logo').src='images/logo2.jpg';
+    document.getElementById('logo').src='imagenes/senati 2.jpg';
 }
 function cambiarFondo(){
      document.getElementById('menu').style.background='#867513ff';
