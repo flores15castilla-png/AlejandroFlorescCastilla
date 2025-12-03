d271ae2e77dde1dbf77af9dda4868247fa189686
Alex <flores15castilla@gmail.com>
2025-12-03 12:56:07 -0500

    s

--- ejemplo.js
+++ ejemplo.js
@@ -4,27 +4,21 @@ alert("Yo lo veo a usted muy bien")
 function bebe(){
     document.getElementById('bebe').src ='imagenes/bebe.png';
 }
-
 function ceniza(){
     document.getElementById('ceniza').src ='imagenes/ceniza.png'
 }
-
 function cambiarColor(){
     document.getElementById('menu').style.color ='#006828ff'
 }
-
 function cambiarLogo(){
     document.querySelector("header img").src ='imagenes/senati 2.jpg'
 }
-
 function cambiarFondo(){
     document.getElementById('menu').style.background ='#867513'
 }
-
 function cambiodeTexto(){
     document.querySelector("h2").textContent ='YA LE SE AL JS'
-}
-
+} 
 function fondoAside(){
     document.getElementById('aside').style.background ='#15ff00'
 }
