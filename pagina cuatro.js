f777f58077f85bae674aa4f513c861f99efe2b18
Alex <flores15castilla@gmail.com>
2025-12-03 20:27:44 -0500

    a

---
+++ pagina cuatro.js
@@ -0,0 +1,34 @@
+console.log("Hola muchacho");
+alert("Yo lo veo a usted muy bien")
+
+function bebe(){
+    document.getElementById('bebe').src ='imagenes/bebe.png';
+    alert("no mano noooo");
+}
+function ceniza(){
+    document.getElementById('ceniza').src ='imagenes/ceniza.png';
+    alert("me quitaste mis derechos T_T");
+}
+function cambiarColor(){
+    document.getElementById('menu').style.color ='#4eb977ff'
+    document.getElementById('men0').style.color='#6e652eff';
+    document.getElementById('men1').style.color='#386638ff';
+    document.getElementById('men2').style.color='#07064dff';
+    document.getElementById('men3').style.color='#68000eff';
+    document.getElementById('men4').style.color='#2dc3ffff';
+    document.getElementById('men5').style.color='#f30be7ff';
+    alert("coformate con este humilde color >:V");
+}
+function cambiarLogo(){
+    document.querySelector('header img').src ='imagenes/senati 2.jpg'
+}
+function cambiarFondo(){
+    document.getElementById('menu').style.background ='#ffe553ff'
+    alert("muchacho deja el racismo")
+}
+function cambiodeTexto(){
+    document.querySelector('h2').textContent ='YA LE SE AL JS'
+} 
+function fondoAside(){
+    document.getElementById('aside').style.background ='#15ff00'
+}
