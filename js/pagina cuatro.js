88902cabd0cedbc48cc6299e40ce6738d906f873
Alex <flores15castilla@gmail.com>
2025-12-08 14:44:35 -0500

    a

--- js/pagina cuatro.js
+++ js/pagina cuatro.js
@@ -1,30 +1,36 @@
 console.log("Hola muchacho");
-alert("Yo lo veo a usted muy bien")
+alert("Yo lo veo a usted muy bien");
 
 function bebe(){
-    document.getElementById('bebe').src ='imagenes/bebe.png';
+    document.getElementById('bebe').src = 'imagenes/bebe.png';
     alert("hola que tal");
 }
+
 function ceniza(){
-    document.getElementById('ceniza').src ='imagenes/ceniza.png';
+    document.getElementById('bebe').src = 'imagenes/ceniza.png';
     alert("me quitaste mis derechos T_T");
 }
+
 function cambiarColor(){
-    document.getElementById('men3').style.color='#68000eff';
-    alert("coformate con este humilde color >:V");
+    document.getElementById('menu').style.color = '#68000e';
+    alert("conformate con este humilde color >:V");
 }
+
 function cambiarLogo(){
-    document.querySelector('header img').src ='imagenes/senati 2.jpg'
-    alert("Te entienndo aveces lo simple es mejor")
+    document.querySelector('header img').src = 'imagenes/senati2.jpg';
+    alert("Te entiendo, aveces lo simple es mejor");
 }
+
 function cambiarFondo(){
-    document.getElementById('secction').style.background ='#ffe553ff'
-    alert("muchacho deja el racismo")
+    document.querySelector('nav').style.background = '#ffe553';
+    alert("muchacho deja el racismo");
 }
+
 function cambiodeTexto(){
-    document.querySelector('Subtitulo').textContent ='DEJA OE'
-    alert("Pareces mujer nada te complace")
+    document.querySelector('header h2').textContent = 'DEJA OE';
+    alert("Pareces mujer nada te complace");
 }
+
 function fondoAside(){
-    document.getElementById('aside').style.background ='#15ff00'
+    document.getElementById('aside').style.background = '#15ff00';
 }
